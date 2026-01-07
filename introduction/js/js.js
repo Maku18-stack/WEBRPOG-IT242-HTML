1ab2b6b6c1e70342cf30b639bf429a695f97beba
Mark Benedict D. Ani <mdani@student.apc.edu.ph>
2026-01-08 03:39:36 +0800

    save as

---
+++ introduction/js/js.js
@@ -0,0 +1,6 @@
+// Simple interaction for the profile page
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Mark's Profile Page Loaded Successfully!");
+    
+    // You can add an alert or a theme toggle here later!
+});
